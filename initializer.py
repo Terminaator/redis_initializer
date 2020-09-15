b7a99bb63320f3359d5876efbaa0f7eec856398e
Paul Liibert <paul.liibert@tieto.com>
2020-09-15 21:23:22 +0300

    cleaning

--- initializer.py
+++ initializer.py
@@ -4,20 +4,20 @@ from oracle_interface import Oracle
 postgres = Postgres()
 oracle = Oracle()
 
-print(postgres.get_building_codes())
-print(oracle.get_building_codes())
+bul_codes_postgres = postgres.get_building_codes()
+bul_codes_oracle = oracle.get_building_codes()
 
 #todo võrdlemine
 #todo commandi tegemine
 
-print(postgres.get_procedure_code())
-print(oracle.get_procedure_code())
+pro_code_postgres = postgres.get_procedure_code()
+pro_code_oracle = oracle.get_procedure_code()
 
 #todo võrdlemine
 #todo commandi tegemine
 
-#print(postgres.get_document_codes())
-#print(oracle.get_document_codes())
+doc_codes_postgres = postgres.get_document_codes()
+doc_codes_oracle = oracle.get_document_codes()
 
 #todo võrdlemine
 #todo commandi tegemine
